11b7ece68f7e9886abe0ab6028d976ab84dd06f7
Narjisha K <144308345+NarjishaK@users.noreply.github.com>
2025-08-06 17:46:04 +0530

    my account

---
+++ src/components/MyAccount/index.tsx
@@ -0,0 +1,279 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import AccountDetails from "./AccountDetails";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import axios from "axios";
+import { BASE_URL } from "@/Helper/handleapi";
+import Swal from "sweetalert2";
+import Signin from "../Auth/Signin";
+
+const MyAccount = () => {
+  const [activeTab, setActiveTab] = useState("addresses");
+  const [accountModal, setAccountModal] = useState(false);
+  //open modal
+  const openModal = () => {
+    setAccountModal(true);
+  };
+  //close modal
+  const closeAccountModal = () => {
+    setAccountModal(false);
+  };
+  const customerDetailsStr = localStorage.getItem("customerDetails");
+  const customerDetails = customerDetailsStr
+    ? JSON.parse(customerDetailsStr)
+    : null;
+
+  const [formData, setFormData] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  });
+  //update password
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handlePasswordChange = async (e) => {
+    e.preventDefault();
+
+    const customerDetailsStr = localStorage.getItem("customerDetails");
+    const customerDetails = customerDetailsStr
+      ? JSON.parse(customerDetailsStr)
+      : null;
+
+    if (!customerDetails?._id) {
+      return alert("Customer not logged in.");
+    }
+
+    try {
+      const response = await axios.put(
+        `${BASE_URL}/customer/update-password/${customerDetails._id}`,
+        formData
+      );
+      Swal.fire({
+        icon: "success",
+        title: "Success!",
+        text: "Password updated successfully!",
+      })
+      setFormData({
+        oldPassword: "",
+        newPassword: "",
+        confirmNewPassword: "",
+      });
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: error.response?.data?.message || "Something went wrong",
+      });
+    }
+  };
+  //check auth
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
+  if (isAuthenticated === null) {
+    return null;
+  }
+
+  return isAuthenticated ? (
+    <>
+      <div className="min-h-screen bg-gray-50 py-8">
+        <section className="overflow-hidden py-20 bg-gray-2">
+          <div className="max-w-[1170px] w-full mx-auto px-4 sm:px-8 xl:px-0">
+            <div className="flex flex-col xl:flex-row gap-7.5">
+              {/* <!-- sidebar start --> */}
+              <div className="xl:max-w-[370px] w-full bg-white rounded-xl shadow-1">
+                <div className="flex xl:flex-col">
+                  <div className="hidden lg:flex flex-wrap items-center gap-5 py-6 px-4 sm:px-7.5 xl:px-9 border-r xl:border-r-0 xl:border-b border-gray-3">
+                    <div className="max-w-[64px] w-full h-16 rounded-full overflow-hidden">
+                      <img
+                        src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000"
+                        alt="user"
+                        width={64}
+                        height={64}
+                      />
+                    </div>
+
+                    <div>
+                      <p className="font-medium text-dark mb-0.5">
+                        {customerDetails?.name}
+                      </p>
+                      <p className="text-custom-xs">{customerDetails?.email}</p>
+                    </div>
+                  </div>
+                  <div className="p-4 sm:p-7.5 xl:p-9">
+                    <div className="flex flex-wrap xl:flex-nowrap xl:flex-col gap-4">
+                      <button
+                        onClick={() => setActiveTab("addresses")}
+                        className={`flex items-center rounded-md gap-2.5 py-3 px-4.5 ease-out duration-200 hover:bg-blue hover:text-white ${
+                          activeTab === "addresses"
+                            ? "text-white bg-blue"
+                            : "text-dark-2 bg-gray-1"
+                        }`}
+                      >
+                        <i className="bi bi-person"></i>
+                        Account Details
+                      </button>
+
+                      <button
+                        onClick={() => setActiveTab("account-details")}
+                        className={`flex items-center rounded-md gap-2.5 py-3 px-4.5 ease-out duration-200 hover:bg-blue hover:text-white ${
+                          activeTab === "account-details"
+                            ? "text-white bg-blue"
+                            : "text-dark-2 bg-gray-1"
+                        }`}
+                      >
+                        <i className="bi bi-key"></i>
+                        Password
+                      </button>
+
+                      <button
+                        onClick={() => {
+                          localStorage.clear();
+                          window.location.href = "/signin";
+                        }}
+                        className={`flex items-center rounded-md gap-2.5 py-3 px-4.5 ease-out duration-200 hover:bg-blue hover:text-white ${
+                          activeTab === "logout"
+                            ? "text-white bg-blue"
+                            : "text-dark-2 bg-gray-1"
+                        }`}
+                      >
+                        <i className="bi bi-box-arrow-left"></i>
+                        Logout
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* <!-- account tab content start --> */}
+              <div
+                className={`xl:max-w-[770px] w-full bg-white rounded-xl shadow-1 ${
+                  activeTab === "addresses" ? "flex" : "hidden"
+                }`}
+              >
+                <div className=" w-full bg-white shadow-1 rounded-xl">
+                  <div className="flex items-center justify-between py-5 px-4 sm:pl-7.5 sm:pr-6 border-b border-gray-3">
+                    <p className="font-medium text-xl text-dark">
+                      Account Details
+                    </p>
+
+                    <button
+                      className="text-dark ease-out duration-200 hover:text-blue"
+                      onClick={openModal}
+                    >
+                      <i className="bi bi-pencil-square"></i>
+                    </button>
+                  </div>
+
+                  <div className="p-4 sm:p-7.5" style={{ width: "100%" }}>
+                    <div
+                      className="flex flex-col gap-4"
+                      style={{ width: "100%" }}
+                    >
+                      <p className="flex items-center gap-2.5 text-custom-sm">
+                        <i className="bi bi-person"></i>
+                        Name: {customerDetails?.name}
+                      </p>
+
+                      <p className="flex items-center gap-2.5 text-custom-sm">
+                        <i className="bi bi-envelope"></i>
+                        Email: {customerDetails?.email}
+                      </p>
+
+                      <p className="flex items-center gap-2.5 text-custom-sm">
+                        <i className="bi bi-telephone"></i>
+                        Phone: {customerDetails?.phone}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* <!--  password tab content start --> */}
+              <div
+                className={`xl:max-w-[770px] w-full ${
+                  activeTab === "account-details" ? "block" : "hidden"
+                }`}
+              >
+                <form onSubmit={handlePasswordChange}>
+                  <p className="font-medium text-xl sm:text-2xl text-dark mb-7">
+                    Password Change
+                  </p>
+
+                  <div className="bg-white shadow-1 rounded-xl p-4 sm:p-8.5">
+                    <div className="mb-5">
+                      <label htmlFor="oldPassword" className="block mb-2.5">
+                        Old Password
+                      </label>
+
+                      <input
+                        type="password"
+                        name="oldPassword"
+                        id="oldPassword"
+                        value={formData.oldPassword}
+                        onChange={handleChange}
+                        autoComplete="on"
+                        className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
+                      />
+                    </div>
+
+                    <div className="mb-5">
+                      <label htmlFor="newPassword" className="block mb-2.5">
+                        New Password
+                      </label>
+
+                      <input
+                        type="password"
+                        name="newPassword"
+                        id="newPassword"
+                        value={formData.newPassword}
+                        onChange={handleChange}
+                        autoComplete="on"
+                        className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
+                      />
+                    </div>
+
+                    <div className="mb-5">
+                      <label
+                        htmlFor="confirmNewPassword"
+                        className="block mb-2.5"
+                      >
+                        Confirm New Password
+                      </label>
+
+                      <input
+                        type="password"
+                        name="confirmNewPassword"
+                        id="confirmNewPassword"
+                        value={formData.confirmNewPassword}
+                        onChange={handleChange}
+                        autoComplete="on"
+                        className="rounded-md border border-gray-3 bg-gray-1 placeholder:text-dark-5 w-full py-2.5 px-5 outline-none duration-200 focus:border-transparent focus:shadow-input focus:ring-2 focus:ring-blue/20"
+                      />
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="inline-flex font-medium text-white bg-blue py-3 px-7 rounded-md ease-out duration-200 hover:bg-blue-dark"
+                    >
+                      Change Password
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+        <AccountDetails isOpen={accountModal} closeModal={closeAccountModal} />
+      </div>
+    </>
+  ) : (
+    <Signin />
+  );
+};
+
+export default MyAccount;
